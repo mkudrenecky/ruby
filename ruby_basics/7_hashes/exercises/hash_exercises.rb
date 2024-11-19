c02b45ca829a28d47755b8cb034709c875f50e41
mkudrenecky <mkudrene@student.ubc.ca>
2024-11-19 11:18:30 -0700

    hash exercises

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -1,18 +1,23 @@
 def create_favorite_hash(color, number)
+  my_hash = {color: color, :number => number}
+my_hash
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
 end
 
 def favorite_color(favorite_list)
+  favorite_list[:color]
   # return the value of the color key
 end
 
 def favorite_number(favorite_list)
+  favorite_list.fetch(:number, 42)
   # use #fetch to return the value of the number key or 42 if the key is not found
 end
 
 def update_favorite_movie(favorite_list, movie)
+  favorite_list[:movie] = movie
   # Step 1: add/update the key of movie (as a symbol)
 
   # Step 2: return the hash (because Step 1 returns the value of the movie key)
@@ -20,6 +25,7 @@ def update_favorite_movie(favorite_list, movie)
 end
 
 def remove_favorite_number(favorite_list)
+  favorite_list.delete(:number)
   # Step 1: delete the number data
 
   # Step 2: return the hash (because Step 1 returns the value of the number key)
@@ -27,13 +33,16 @@ def remove_favorite_number(favorite_list)
 end
 
 def favorite_categories(favorite_list)
+  favorite_list.keys()
   # return the keys of favorite_list
 end
 
 def favorite_items(favorite_list)
+  favorite_list.values()
   # return the values of favorite_list
 end
 
 def merge_favorites(original_list, additional_list)
+  original_list.merge(additional_list)
   # merge the two hashes: original_list and additional_list
 end
